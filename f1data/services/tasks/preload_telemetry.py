cb1f430ee01579e030ba29099c303c67af096257
German <thewhitehaven04@gmail.com>
2025-03-15 22:55:04 +0100

    Fix prefetching

--- f1data/services/tasks/preload_telemetry.py
+++ f1data/services/tasks/preload_telemetry.py
@@ -1,7 +1,11 @@
+from fastapi.concurrency import run_in_threadpool
 from core.models.queries import SessionIdentifier
 from services.session.session import get_loader
 
 
-async def preload_telemetry(year: str, round: str, session: SessionIdentifier | int, is_testing: bool):
+async def preload_telemetry(
+    year: str, round: str, session: SessionIdentifier | int, is_testing: bool
+):
     loader = get_loader(year, int(round), session, is_testing)
-    await loader._fetch_lap_telemetry()
+    async with loader.telemetry_lock:
+        await run_in_threadpool(loader._fetch_lap_telemetry)
